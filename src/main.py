231d8fd20f8a2482cb4bfa92c635d2b21156d8c8
Nurbek Kuspanov <nurbek@inbox.ru>
2025-07-31 00:15:05 +0400

    mocked API /users/me

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 
 FRONTEND_DIR = Path(__file__).parent / "frontend"
 
 
 app = FastAPI()
+
+
+@app.get("/users/me")
+def mock_get_user():
+    mock_user_data = {
+        "profileId": 0,
+        "email": "user@example.com",
+        "username": "string",
+        "registeredAt": "string",
+        "updatedAt": "string",
+        "isActive": True,
+    }
+    return JSONResponse(content=mock_user_data, status_code=200)
 
 
 app.mount(
