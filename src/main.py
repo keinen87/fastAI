4d7b463cebd038e8bc1b4373a158a9e40675b446
Nurbek Kuspanov <nurbek@inbox.ru>
2025-09-09 17:44:43 +0400

    mocked APIs site generate

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import asyncio
 from pathlib import Path
+from typing import Annotated
 
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import Body, FastAPI, Path as pth
+from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 FRONTEND_DIR = Path(__file__).parent / "frontend"
 
@@ -11,13 +13,14 @@ FRONTEND_DIR = Path(__file__).parent / "frontend"
 app = FastAPI()
 
 
-class UserProfile(BaseModel):
+class UserProfileResponse(BaseModel):
     email: str
     is_active: bool = True
     profile_id: str
     registered_at: str
     updated_at: str
     username: str
+
     model_config = ConfigDict(json_schema_extra={
         "examples": [{
             "email": "user@example.com",
@@ -30,17 +33,109 @@ class UserProfile(BaseModel):
     })
 
 
-@app.get("/users/me", response_model=UserProfile)
+SiteTitle = Annotated[
+    str,
+    Field(min_length=4),
+    Field(description="Название веб-страницы в поисковой выдаче и на вкладке браузера"),
+]
+
+mock_site_schema = {
+    "created_at": "2025-06-15T18:29:56+00:00",
+    "html_code_download_url": "http://example.com/media/index.html?response-content-disposition=attachment",
+    "html_code_url": "http://example.com/media/index.html",
+    "id": 1,
+    "prompt": "Сайт любителей играть в домино",
+    "screenshot_url": "http://example.com/media/index.png",
+    "title": "Фан клуб Домино",
+    "updated_at": "2025-06-15T18:29:56+00:00",
+}
+
+mock_user_data = {
+    "email": "user@example.com",
+    "is_active": True,
+    "profile_id": "0",
+    "registered_at": "2025-06-15T18:29:56+00:00",
+    "updated_at": "2025-06-15T18:29:56+00:00",
+    "username": "user123",
+}
+
+
+class SiteSchemaResponse(BaseModel):
+    created_at: str
+    html_code_download_url: str
+    html_code_url: str
+    id: int
+    prompt: str
+    screenshot_url: str
+    title: SiteTitle
+    updated_at: str
+
+    model_config = ConfigDict(json_schema_extra={
+        "examples": [mock_site_schema],
+    })
+
+
+class SiteCreate(BaseModel):
+    prompt: str
+    title: SiteTitle
+    model_config = ConfigDict(json_schema_extra={
+        "examples": [{
+            "prompt": "Сайт любителей играть в домино",
+            "title": "Фан клуб игры в домино",
+        }],
+    })
+
+
+class SiteGenerate(BaseModel):
+    prompt: str
+    model_config = ConfigDict(json_schema_extra={
+        "examples": [{
+            "prompt": "Сайт любителей играть в домино",
+        }],
+    })
+
+
+@app.get("/users/me", response_model=UserProfileResponse, summary="Получить учётные данные пользователя")
 def mock_get_user_profile():
-    mock_user_data = {
-        "email": "user@example.com",
-        "is_active": True,
-        "profile_id": "0",
-        "registered_at": "2025-06-15T18:29:56+00:00",
-        "updated_at": "2025-06-15T18:29:56+00:00",
-        "username": "user123",
-    }
     return JSONResponse(content=mock_user_data, status_code=200)
+
+
+@app.get("/sites/my",
+         response_model=SiteSchemaResponse,
+         summary="Получить список сгенерированных сайтов текущего пользователя")
+def mock_get_my_site():
+    return JSONResponse(content=mock_site_schema, status_code=200)
+
+
+@app.post("/sites/create", response_model=SiteSchemaResponse, summary="Создать сайт")
+def create_site(site_data: SiteCreate):
+    return JSONResponse(content=mock_user_data, status_code=200)
+
+
+async def get_mock_site():
+    block_size = 1024
+    seconds = 3
+    with open("index.html", encoding="utf8") as file:
+        while chunk := file.read(block_size):
+            await asyncio.sleep(seconds)
+            yield chunk
+
+
+@app.post("/sites/{site_id}/generate",
+          summary="Сгенерировать HTML код сайта",
+          description="Код сайта будет транслироваться стримом по мере генерации.")
+async def generate_site(site_id: int = pth(..., gt=0, title="ID сайта",
+                        description="Должен быть положительным"),
+                        site_data: SiteGenerate = Body(...)):
+    return StreamingResponse(
+        content=get_mock_site(),
+        media_type="text/html",
+    )
+
+
+@app.get("/sites/{site_id}", response_model=SiteSchemaResponse, summary="Получить сайт")
+def get_user(site_id: int = pth(..., gt=0, title="ID сайта", description="Должен быть положительным")):
+    return JSONResponse(content=mock_site_schema, status_code=200)
 
 
 app.mount(
